db760c0ef22818a5aa7caa2fde8beb54739b4535
Pedro Medeiros <pedrosnk@gmail.com>
2015-03-23 05:50:29 -0300

    add uglify to stack

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
 
-gulp.task('default', function(){
-    gulp.src(['js/modules/*.js', 'js/directives/*.js'])
+gulp.task('default', ['build']);
+
+gulp.task('build', function(){
+    gulp.src(['./js/modules/*.js', './js/directives/*.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
         .pipe(jshint.reporter('fail'))
         .pipe(concat('dblist.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(concat('dblist.min.js'))
+        .pipe(uglify())
         .pipe(gulp.dest('dist'));
 });
+
